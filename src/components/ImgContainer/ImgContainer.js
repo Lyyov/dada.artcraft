e7e05e2a0effb7483accaa6101907215494a35c8
Lyyov <54508812+Lyyov@users.noreply.github.com>
2022-05-13 22:24:46 +0300

    progressive loading images

--- src/components/ImgContainer/ImgContainer.js
+++ src/components/ImgContainer/ImgContainer.js
@@ -1,20 +1,37 @@
 import { Fragment } from "react";
+import ProgressiveImage from "react-progressive-graceful-image";
 
-const ImgContainer = ({id, src, className, alt, zoomHandler}) => {
-  const cls = 'image-container';
-  
+const ImgContainer = ({ id, src, className, alt, zoomHandler }) => {
+  const cls = "image-container";
+
+  const dominantImageColor = "#b5e2d2";
+
+  const placeholder = (
+    <div
+      style={{ backgroundColor: dominantImageColor, height: 500, opacity: 0.2 }}
+    />
+  );
+
   return (
     <Fragment>
       <div className={!className ? cls : `${cls} ${className}`}>
-        <img
-          src={src} 
-          className="image-container__image"  
-          alt={alt} 
-          onClick={zoomHandler ? () => zoomHandler(id) : null} />
+        <ProgressiveImage src={src} placeholder="">
+          {(src, loading) => {
+            return loading ? (
+              placeholder
+            ) : (
+              <img
+                className="image-container__image"
+                src={src}
+                alt={alt}
+                onClick={zoomHandler ? () => zoomHandler(id) : null}
+              />
+            );
+          }}
+        </ProgressiveImage>
       </div>
-      
     </Fragment>
-  )
-}
+  );
+};
 
 export default ImgContainer;
