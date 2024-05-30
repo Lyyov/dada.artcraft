c4cca38634a1b72e966c905dfbab6665bee71e1d
Lyyov <lyyovghidor@gmail.com>
2024-05-30 17:10:31 +0300

    Adjustments on service page.

--- app/services/_components/Service.tsx
+++ app/services/_components/Service.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export type TService = {
   img: string;
   title: string;
@@ -7,7 +9,13 @@ export default function Service({ title, img, text }: TService) {
   return (
     <div className="col-md-3" key={title}>
       <div className="services__list-item">
-        <img loading="lazy" src={img} alt="service" />
+        <Image
+          loading="lazy"
+          src={img}
+          alt={`Service ${title}`}
+          width={305}
+          height={376}
+        />
         <div className="services__list-content">
           <div className="services__list-title">{title}</div>
           <p className="services__list-text">{text}</p>
