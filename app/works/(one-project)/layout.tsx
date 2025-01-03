f6132224d138ede755b84cf52a98565d4f1fb8fb
Lyyov <mymac@Mac-mini-MyMac.local>
2025-01-03 23:13:38 +0200

    Init next 15.

--- app/works/(one-project)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { OneProjectProvider } from "./_components/ModalContext";
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <OneProjectProvider>{children}</OneProjectProvider>;
-}
