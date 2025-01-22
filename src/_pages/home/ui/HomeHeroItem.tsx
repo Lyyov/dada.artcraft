742f7a3e0d1a503de1bd44c94a6c430ac6e1766c
Lyyov <54508812+Lyyov@users.noreply.github.com>
2025-01-22 14:59:56 +0200

    Fix arrows alignment.

--- src/_pages/home/ui/HomeHeroItem.tsx
+++ src/_pages/home/ui/HomeHeroItem.tsx
@@ -1,17 +1,29 @@
+import clsx from "clsx";
+
 interface IHomeHeroItem {
-    text1?: string;
-    text2: string;
+  text1?: string;
+  text2: string;
 }
 export const HomeHeroItem = ({ text1, text2 }: IHomeHeroItem) => (
-    <div className="relative text-center flex items-center justify-center">
-        {text1 && (
-            <>
-                <span className={`uppercase text-center text-[30px] md:text-[40px] font-bold mb-1.5 text-foreground-secondary`}>
-                    {text1}
-                </span>
-                <br />
-            </>
+  <div className="min-h-[86px_!important] md:min-h-[145px_!important] justify-center items-center">
+    <div className="relative text-center ">
+      {text1 && (
+        <>
+          <span
+            className={`uppercase text-center text-[30px] md:text-[40px] font-bold mb-1.5 text-foreground-secondary`}
+          >
+            {text1}
+          </span>
+          <br />
+        </>
+      )}
+      <span
+        className={clsx(
+          `uppercase text-center text-[40px] font-bold text-foreground-secondary md:text-[85px] leading-[1]`
         )}
-        <span className={`uppercase text-center text-[40px] font-bold text-foreground-secondary md:text-[85px] leading-[1]`}>{text2}</span>
+      >
+        {text2}
+      </span>
     </div>
-)
+  </div>
+);
